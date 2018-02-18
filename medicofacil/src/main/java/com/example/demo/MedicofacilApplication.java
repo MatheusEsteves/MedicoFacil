f229d2174b6f1417f6ddbb62c36ff9017ec97021
Matheus Esteves Zanoto <matheusesteveszanoto@gmail.com>
2018-02-18 17:26:28 -0300

    Migrando de Maven Project para Spring Project

---
+++ medicofacil/src/main/java/com/example/demo/MedicofacilApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicofacilApplication.class, args);
+	}
+}
